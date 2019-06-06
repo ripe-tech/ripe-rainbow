79a986e841340c014f648cd20abe7a4a026c0c2b
joamag <joamag@gmail.com>
2019-06-06 13:01:28 +0100

    Initial color and emoji support

--- src/ripe_rainbow/runners.py
+++ src/ripe_rainbow/runners.py
@@ -49,12 +49,23 @@ class ConsoleRunner(Runner):
         # sets the default result flag value to valid as the execution
         # is considered to be valid by default
         result = True
+        passed = 0
+        failed = 0
+
+        # prints the hader information on the product to be used to indicate
+        # the proper exeuction of then console
+        print("RIPE Rainbow 🌈")
+        print("")
 
         try:
             # iterates over the complete set of test cases for the test
             # suite that has been provided by the current loader to run
             # their respective tests, under the current runner
             for test_case in self.test_suite:
+
+                # sets the printed (overhead) flag as false indicating that
+                # the name of the class is still pending printing
+                printed = False
 
                 # iterates over the complete set of test (methods) in the
                 # test case to run then (if they are required)
@@ -66,6 +77,12 @@ class ConsoleRunner(Runner):
                     if self.regex and not self.regex.match(test_name):
                         continue
 
+                    # in case the name of the test case class is not printed
+                    # runs the printing operation and unsets the flag
+                    if not printed:
+                        print("    %s" % test_case.__class__.__name__)
+                        printed = True
+
                     # flushes the stdout and stderr so that the pending
                     # messages are properly handled by the output channels
                     sys.stdout.flush()
@@ -73,21 +90,51 @@ class ConsoleRunner(Runner):
 
                     try:
                         with appier_console.ctx_loader(
-                            template = "{{spinner}} Running test: %s" % test_name
+                            template = "        %s {{spinner}}" % test_name,
+                            end_newline = False
                         ):
                             test_case.run_test(test)
+
+                        test_name_s = appier_console.colored(test_name, color = appier_console.COLOR_GRAY)
+                        print("        %s ✔️" % test_name_s)
+                        passed += 1
                     except appier.AssertionError as exception:
                         result = False
-                        print("There was an error: %s" % str(exception))
+                        test_name_s = appier_console.colored(test_name, color = appier_console.COLOR_RED)
+                        print("        %s ❌️" % test_name_s)
+                        #print("There was an error: %s" % str(exception))
+                        failed += 1
                     except Exception as exception:
                         result = False
-                        print("There was an exception: %s" % str(exception))
-                        print("Exception in user code:")
-                        print("-" * 60)
-                        traceback.print_exc(file = sys.stdout)
-                        print("-" * 60)
+                        #print("There was an exception: %s" % str(exception))
+                       # print("Exception in user code:")
+                       # print("-" * 60)
+                       # traceback.print_exc(file = sys.stdout)
+                       # print("-" * 60)
+                        test_name_s = appier_console.colored(test_name, color = appier_console.COLOR_RED)
+                        print("        %s ❌️" % test_name_s)
+                        failed += 1
+
+                if printed: print("")
         finally:
             self.run_on_finish()
+
+        passed_s = appier_console.colored(
+            "%d passing" % passed,
+            color = appier_console.COLOR_GREEN
+        ) if passed else ""
+        failed_s = appier_console.colored(
+            "%d failing" % failed,
+            color = appier_console.COLOR_RED
+        ) if failed else ""
+        print("  %s %s" % (passed_s, failed_s))
+
+        print("")
+
+        if result:
+            print("The sky is blue and the sun shining ☀️")
+        else:
+            print("There are some clouds in the sky 🌧️")
 
         return result
 
