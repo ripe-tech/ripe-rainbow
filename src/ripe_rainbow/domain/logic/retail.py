882399d01910cb551fdd59c3dc7c7c447933763e
joamag <joamag@hive.pt>
2019-06-19 19:36:51 +0100

    Fixed a lot of code stuff

--- src/ripe_rainbow/domain/logic/retail.py
+++ src/ripe_rainbow/domain/logic/retail.py
@@ -25,10 +25,12 @@ class RetailPart(parts.Part):
 
         self.waits.redirected_to(self.base_url)
 
-    def select_size(self, size, gender = None, scale = None, already_open = False):
+    def select_size(self, size, gender = None, scale = None, open = True):
         """
         Opens the size selection window, selects the proper scale and size and
         applies that configuration by clicking 'Apply' and closing the window.
+
+        Notice that if the "open" flag is unset the window is not opened.
 
         :type size: String
         :param size: The size to be picked.
@@ -40,7 +42,7 @@ class RetailPart(parts.Part):
         :param already_open: Whether the size modal is already open.
         """
 
-        if not already_open: self.interactions.click_when_possible(".size:not(.disabled) .button-size")
+        if open: self.interactions.click_when_possible(".size:not(.disabled) .button-size")
 
         if gender:
             self.interactions.click_when_possible(
@@ -63,15 +65,15 @@ class RetailPart(parts.Part):
         self.waits.is_not_visible(".size .modal")
 
     def set_part(
-            self,
-            brand,
-            model,
-            part,
-            material,
-            color,
-            part_text = None,
-            material_text = None,
-            color_text = None
+        self,
+        brand,
+        model,
+        part,
+        material,
+        color,
+        part_text = None,
+        material_text = None,
+        color_text = None
     ):
         """
         Makes a change to the customization of a part and checks that the pages
@@ -88,15 +90,18 @@ class RetailPart(parts.Part):
         :param material: The technical name of the material to use for the part.
         :type color: String
         :param color: The technical name of the color to use for the part.
-        :type part: String
-        :param part: The expected label for the part after clicking.
-        :type material: String
-        :param part: The expected label for the material after clicking.
-        :type color: String
-        :param part: The expected label for the color after clicking.
+        :type part_text: String
+        :param part_text: The expected label for the part after clicking.
+        :type material_text: String
+        :param material_text: The expected label for the material after clicking.
+        :type color_text: String
+        :param color_text: The expected label for the color after clicking.
         """
 
-        self.interactions.click_when_possible(".pickers .button-part", condition = lambda e: e.text == part.upper())
+        self.interactions.click_when_possible(
+            ".pickers .button-part",
+            condition = lambda e: e.text == part.upper()
+        )
         if part_text: self.waits.text(".button-part.active", part_text)
 
         self.interactions.click_when_possible(".pickers .button-color[data-color='%s']" % color)
@@ -105,24 +110,38 @@ class RetailPart(parts.Part):
         if material_text: self.waits.text(".button-material.active", material_text)
 
         self.waits.until(
-            lambda d: self.assert_swatch(".pickers .button-part.active .swatch > img", brand, model, material, color),
+            lambda d: self.assert_swatch(
+                ".pickers .button-part.active .swatch > img",
+                brand, model, material, color
+            ),
             "Part swatch didn't have the expected image."
         )
         self.waits.until(
-            lambda d: self.assert_swatch(".pickers .button-color.active .swatch > img", brand, model, material, color),
+            lambda d: self.assert_swatch(
+                ".pickers .button-color.active .swatch > img",
+                brand, model, material, color
+            ),
             "Color swatch didn't have the expected image."
         )
 
     def assert_swatch(self, selector, brand, model, material, color):
         """
-        Checks that the img element identified by the selector points to the correct swatch.
+        Checks that the img element identified by the selector points to the
+        correct swatch.
 
+        This kind of assertion is critical to ensure proper responsiveness of
+        the UI in accordance with part selection.
+
+        :type selector: String
         :param selector: The selector for the img.
+        :type brand: String
         :param brand: The brand of the swatch.
+        :type model: String
         :param model: The model of the swatch.
+        :type material: String
         :param material: The material the swatch should represent.
+        :type color: String
         :param color: The color being shown in the shown.
-        :return: Nothing.
         """
 
         element = self.waits.element(selector)
