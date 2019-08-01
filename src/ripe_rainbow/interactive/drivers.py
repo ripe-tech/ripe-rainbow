bb073adea01742258f2e2edadf449a0aaad1f21f
joamag <joamag@hive.pt>
2019-08-01 11:28:38 +0100

    Removed extra posibility

--- src/ripe_rainbow/interactive/drivers.py
+++ src/ripe_rainbow/interactive/drivers.py
@@ -392,7 +392,6 @@ class SeleniumDriver(InteractiveDriver):
         # for the offset operation to move the cursor outside of the
         # requested element (avoiding collision)
         possibilities = (
-            (-1, -1),
             (-1, 0),
             (0, -1),
             (width, -1),
