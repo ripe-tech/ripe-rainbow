0c10d5c1ad66460112b4e61290eda3c7160f7a22
joamag <joamag@hive.pt>
2019-08-01 16:07:18 +0100

    New version 0.5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "ripe-rainbow",
-    version = "0.4.21",
+    version = "0.5.0",
     author = "Platforme International",
     author_email = "development@platforme.com",
     description = "RIPE Rainbow",
--- src/ripe_rainbow/info.py
+++ src/ripe_rainbow/info.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 NAME = "RIPE Rainbow"
-VERSION = "0.4.21"
+VERSION = "0.5.0"
 RAINBOW = "🌈"
 LABEL = "%s %s %s" % (NAME, VERSION, RAINBOW)
