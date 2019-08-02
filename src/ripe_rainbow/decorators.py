54adb1a07b0203d762a4d7764c3e7e6f2a40f12d
joamag <joamag@hive.pt>
2019-08-02 11:41:10 +0100

    Small fallback for bug

--- src/ripe_rainbow/decorators.py
+++ src/ripe_rainbow/decorators.py
@@ -6,8 +6,15 @@ import functools
 def test(
     description = None,
     script_url = None,
+    bug = None,
     bugs = []
 ):
+
+    # in case the bugs are not defined tries to fallback
+    # to the (single) bug parameter (convenience) and then
+    # ensures that the bugs are set as a sequence
+    if not bugs: bugs = [bug] if bug else bugs
+    if not isinstance(bugs, (list, tuple)): bugs = [bugs]
 
     def decorator(function):
 
