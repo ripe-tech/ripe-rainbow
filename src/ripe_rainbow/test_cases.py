670df1943fc7ee2f9a33071b36de1fbf34d30b4e
joamag <joamag@gmail.com>
2019-11-20 10:24:28 +0000

    Small fix

--- src/ripe_rainbow/test_cases.py
+++ src/ripe_rainbow/test_cases.py
@@ -29,7 +29,7 @@ class TestCase(appier.Observable):
         level = appier.conf("LEVEL", level)
         level = appier.conf("RAINBOW_LEVEL", level)
         level = logging.getLevelName(level.upper())
-        formatter = logging.Formatter("[breadcrumbs] %%(asctime)s [%%(levelname)s] %s%%(message)s")
+        formatter = logging.Formatter("[breadcrumbs] %(asctime)s [%(levelname)s] %(message)s")
         logger = logging.getLogger(name)
         handler = logging.StreamHandler()
         logger.addHandler(handler)
