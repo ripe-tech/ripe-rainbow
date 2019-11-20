32d8c8520845d030a72ddfadf3bc63bc9536c197
joamag <joamag@gmail.com>
2019-11-20 09:44:21 +0000

    New version 0.6.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "ripe-rainbow",
-    version = "0.6.4",
+    version = "0.6.5",
     author = "Platforme International",
     author_email = "development@platforme.com",
     description = "RIPE Rainbow",
