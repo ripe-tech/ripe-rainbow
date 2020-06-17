a1c638041048e9db7c64d40c656513c63855d4f6
João Magalhães <joamag@gmail.com>
2020-06-17 12:41:42 +0100

    Version 0.7.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "ripe-rainbow",
-    version = "0.7.12",
+    version = "0.7.13",
     author = "Platforme International",
     author_email = "development@platforme.com",
     description = "RIPE Rainbow",
