cad2d4e9cb88a721fe9f7749ce4ee36e8f150d30
Gabriel Candal <gcc@platforme.com>
2020-06-26 12:11:39 +0100

    Reorder and add missing methods

--- src/ripe_rainbow/domain/logic/ripe_white.py
+++ src/ripe_rainbow/domain/logic/ripe_white.py
@@ -41,17 +41,17 @@ class RipeWhitePart(parts.Part):
     def select_part(self, part):
         self.interactions.click(".content .pickers .button-part[data-part='%s']" % part)
 
-    def select_material(self, material):
-        self.interactions.click(".content .pickers .button-material[data-material='%s']" % material)
-
-    def select_color(self, material, color):
-        self.interactions.click(".content .pickers .button-color-option[data-material='%s'][data-color='%s']" % (material, color))
-
     def select_part_mobile(self, part):
         self.interactions.click(".content-mobile .pickers .button-part[data-part='%s']" % part)
 
+    def select_material(self, material):
+        self.interactions.click(".content .pickers .button-material[data-material='%s']" % material)
+
     def select_material_mobile(self, material):
         self.interactions.click(".content-mobile .pickers .button-material[data-material='%s']" % material)
+
+    def select_color(self, material, color):
+        self.interactions.click(".content .pickers .button-color-option[data-material='%s'][data-color='%s']" % (material, color))
 
     def select_color_mobile(self, material, color):
         self.interactions.click(".content-mobile .pickers .button-color-option[data-material='%s'][data-color='%s']" % (material, color))
@@ -64,14 +64,31 @@ class RipeWhitePart(parts.Part):
             timeout = timeout
         )
 
+    def assert_no_part_mobile(self, part, timeout = None):
+        self.waits.not_visible(
+            ".content-mobile .pickers .button-part > p",
+            text = self._capitalize_words(part),
+            message = "The selector for the part '%s' didn't disappear" % part,
+            timeout = timeout
+        )
+
     def assert_no_material(self, part, material):
         self.select_part(part)
         self.waits.not_visible(".material li[data-material='%s']" % material)
         self.waits.not_visible(".content .pickers .button-color[data-material='%s']" % material)
 
+    def assert_no_material_mobile(self, part, material):
+        self.select_part_mobile(part)
+        self.waits.not_visible(".material li[data-material='%s']" % material)
+        self.waits.not_visible(".content-mobile .pickers .button-color[data-material='%s']" % material)
+
     def assert_no_color(self, part, color):
         self.select_part(part)
         self.waits.not_visible(".content .pickers .button-color[data-color='%s']" % color)
+
+    def assert_no_color_mobile(self, part, color):
+        self.select_part_mobile(part)
+        self.waits.not_visible(".content-mobile .pickers .button-color[data-color='%s']" % color)
 
     def set_part(
         self,
@@ -123,6 +140,68 @@ class RipeWhitePart(parts.Part):
 
         if verify:
             self.assert_part(
+                brand,
+                model,
+                part,
+                material,
+                color,
+                part_text = part_text,
+                material_text = material_text,
+                color_text = color_text,
+                has_swatch = has_swatch,
+                select_part = False
+            )
+
+    def set_part_mobile(
+        self,
+        brand,
+        model,
+        part,
+        material,
+        color,
+        part_text = None,
+        material_text = None,
+        color_text = None,
+        verify = True,
+        has_swatch = True
+    ):
+        """
+        Makes a change to the customization of a part and checks that the pages
+        mutates correctly, picking the right active parts, materials and colors,
+        as well as properly switching the swatches.
+
+        If the text parameters are passed an extra set of assertions are going
+        to be performed to validate expected behaviour.
+
+        :type brand: String
+        :param brand: The brand of the model being customized.
+        :type model: String
+        :param model: The model being customized.
+        :type part: String
+        :param part: The technical name of the part being changed.
+        :type material: String
+        :param material: The technical name of the material to use for the part.
+        :type color: String
+        :param color: The technical name of the color to use for the part.
+        :type part_text: String
+        :param part_text: The expected label for the part after clicking.
+        :type material_text: String
+        :param material_text: The expected label for the material after clicking.
+        :type color_text: String
+        :param color_text: The expected label for the color after clicking.
+        :type verify: bool
+        :param verify: If a final assertion should be performed after the selection
+        has been done (to verify the final status).
+        :type has_swatch: Boolean
+        :param has_swatch: Whether there should be a swatch.
+        """
+
+        self.select_part_mobile(part)
+        self.select_material_mobile(material)
+        self.select_color_mobile(material, color)
+
+        if verify:
+            self.assert_part_mobile(
                 brand,
                 model,
                 part,
@@ -205,68 +284,6 @@ class RipeWhitePart(parts.Part):
                     brand, model, material, color
                 ),
                 "Color swatch didn't have the expected image"
-            )
-
-    def set_part_mobile(
-        self,
-        brand,
-        model,
-        part,
-        material,
-        color,
-        part_text = None,
-        material_text = None,
-        color_text = None,
-        verify = True,
-        has_swatch = True
-    ):
-        """
-        Makes a change to the customization of a part and checks that the pages
-        mutates correctly, picking the right active parts, materials and colors,
-        as well as properly switching the swatches.
-
-        If the text parameters are passed an extra set of assertions are going
-        to be performed to validate expected behaviour.
-
-        :type brand: String
-        :param brand: The brand of the model being customized.
-        :type model: String
-        :param model: The model being customized.
-        :type part: String
-        :param part: The technical name of the part being changed.
-        :type material: String
-        :param material: The technical name of the material to use for the part.
-        :type color: String
-        :param color: The technical name of the color to use for the part.
-        :type part_text: String
-        :param part_text: The expected label for the part after clicking.
-        :type material_text: String
-        :param material_text: The expected label for the material after clicking.
-        :type color_text: String
-        :param color_text: The expected label for the color after clicking.
-        :type verify: bool
-        :param verify: If a final assertion should be performed after the selection
-        has been done (to verify the final status).
-        :type has_swatch: Boolean
-        :param has_swatch: Whether there should be a swatch.
-        """
-
-        self.select_part_mobile(part)
-        self.select_material_mobile(material)
-        self.select_color_mobile(material, color)
-
-        if verify:
-            self.assert_part_mobile(
-                brand,
-                model,
-                part,
-                material,
-                color,
-                part_text = part_text,
-                material_text = material_text,
-                color_text = color_text,
-                has_swatch = has_swatch,
-                select_part = False
             )
 
     def assert_part_mobile(
