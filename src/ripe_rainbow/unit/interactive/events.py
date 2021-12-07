4ca975612a3c92515d9227d8d6c3d8a241888ba6
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-07 16:49:24 +0000

    refactor: reorder by alphabetical order

--- src/ripe_rainbow/unit/interactive/events.py
+++ src/ripe_rainbow/unit/interactive/events.py
@@ -16,25 +16,6 @@ class EventsTest(unittest.TestCase):
         stringifier = ripe_rainbow.interactive.events.EVENT_STRINGIFIERS[event]
         self.assertEqual(stringifier, ripe_rainbow.interactive.events.stringify_response_received)
 
-    def test_stringify_request_will_be_sent(self):
-        message_j = dict(
-            method = "Network.requestWillBeSent",
-            params = dict(
-                requestId = "3D8A8C5C9832597801E0",
-                timestamp = 1987.03854,
-                request = dict(
-                    method = "GET",
-                    url = "https://platforme.com",
-                    headers = {
-                        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64)"
-                    }
-                )
-            )
-        )
-        log = ripe_rainbow.interactive.events.stringify_request_will_be_sent(message_j)
-        expected = "Network.requestWillBeSent 3D8A8C5C9832597801E0 GET https://platforme.com {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}"
-        self.assertEqual(log, expected)
-
     def test_stringify_response_received(self):
         message_j = dict(
             method = "Network.responseReceived",
@@ -54,4 +35,23 @@ class EventsTest(unittest.TestCase):
         )
         log = ripe_rainbow.interactive.events.stringify_response_received(message_j)
         expected = "Network.responseReceived 3D8A8C5C9832597801E08A06FA29E4C0 200 https://platforme.com {'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/html'}"
+        self.assertEqual(log, expected)
+
+    def test_stringify_request_will_be_sent(self):
+        message_j = dict(
+            method = "Network.requestWillBeSent",
+            params = dict(
+                requestId = "3D8A8C5C9832597801E0",
+                timestamp = 1987.03854,
+                request = dict(
+                    method = "GET",
+                    url = "https://platforme.com",
+                    headers = {
+                        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64)"
+                    }
+                )
+            )
+        )
+        log = ripe_rainbow.interactive.events.stringify_request_will_be_sent(message_j)
+        expected = "Network.requestWillBeSent 3D8A8C5C9832597801E0 GET https://platforme.com {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}"
         self.assertEqual(log, expected)
