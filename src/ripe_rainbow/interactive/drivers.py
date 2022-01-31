0e693d91a1c5d2eaa4a5e4a54d931bfba39061b1
João Magalhães <joamag@gmail.com>
2022-01-31 12:48:21 +0000

    feat: better storage protocol validation

--- src/ripe_rainbow/interactive/drivers.py
+++ src/ripe_rainbow/interactive/drivers.py
@@ -66,12 +66,13 @@ class InteractiveDriver(object):
         raise appier.NotImplementedError()
 
     def clear_storage(self):
-        self.clear_cookies()
+        # determines if the current page protocol is "compatible" with
+        # local storage (allow proper exception prevention)
+        storage_protocol = self.instance.execute_script("return !['data:'].includes(location.protocol);")
 
-        has_storage = self.instance.execute_script("return location.protocol !== 'data:';")
-        if has_storage:
-            self.clear_local_storage()
-            self.clear_session_storage()
+        self.clear_cookies()
+        if storage_protocol: self.clear_local_storage()
+        if storage_protocol: self.clear_session_storage()
 
     def clear_cookies(self):
         raise appier.NotImplementedError()
